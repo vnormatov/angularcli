2a7f0d0dd72dac939cb143b266f8bb17df903275
normatovv <normatovv@edi.su>
2017-09-25 12:41:26 +0300

    36

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,20 +2,20 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: 'server.component.html'
+  templateUrl: 'server.component.html',
+  styleUrls: ['server.component.css']
 })
 export class ServerComponent {
 
   serverId = 10;
   private serverStatus = 'offline';
-  public color = '';
 
   constructor() {
-    this.color = Math.random() > 0.5 ? 'red' : 'green';
+    this.serverStatus = Math.random() > 0.5 ? 'offline' : 'online';
   }
 
   public getColor() {
-    return this.color;
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 
   public getServerStatus(): string {
